54f439bce1c16354ac2ec4b29f78f1588dee2641
justin57754 <jusmarr@gmail.com>
2022-07-16 22:16:46 -0700

    removed things haha funny

--- Scripts/build_proto_for_cs.ps1
+++
@@ -1,5 +0,0 @@
-if (Test-Path 'CS') {
-    Remove-Item 'CS' -Recurse
-}
-New-Item -Path 'CS' -ItemType Directory 
-protoc -I="../" "../*.proto" --csharp_out="CS"
--- Scripts/build_proto_for_cs.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-protogen --proto_path=. --csharp_out=. +oneof=enum ./*.proto
